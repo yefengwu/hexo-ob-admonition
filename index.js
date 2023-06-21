70e3ee783d14edf088fda5ed15170142c14dd55f
xuwei <franklxw@outlook.com>
2023-06-21 16:00:29 +0800

    feat: Implement Obsidian admonition rendering in Hexo

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var md = require('@upupming/hexo-renderer-markdown-it-plus/node_modules/markdown-it')();
+md.use(require('@neilsustc/markdown-it-katex'));
+md.use(require('markdown-it-deflist'));
+md.use(require('markdown-it-emoji'));
+md.use(require('markdown-it-footnote'));
+md.use(require('markdown-it-mark'));
+md.use(require('markdown-it-sub'));
+md.use(require('markdown-it-toc'));
+
+const path = require('path')
+const fs = require('hexo-fs')
+const STYLE_PATH = path.resolve(__dirname, './style.css')
+
+hexo.extend.filter.register('before_post_render', function (data) {
+  const style_content = fs.readFileSync(STYLE_PATH).toString()
+
+  let strData;
+  let strRegExp = /(\s*)(```) *ad-(note|info|todo|warning|attention|caution|failure|missing|fail|error) *\n?(.*?)\n([\s\S]+?)\s*(\2)(\n+|$)/g
+
+  if (strRegExp.test(data.content)) {
+    strData = data.content.replace(strRegExp, function (raw, start, startQuote, type, title, content, endQuote, end) {
+      if (title.startsWith("title: ")) {
+          title = title.slice("title: ".length);
+      }
+      let result = '\n\n<style>' + style_content + '</style>\n\n' +  
+              '<div class="admonition ' + type.toLowerCase() + '"><p class="admonition-title">' + title + '</p>' + 
+              '<p class= "admonition-content">' + content + '</p>\n\n' + '</div>\n\n';
+      return result;
+    });
+    data.content = strData;
+  }
+  return data;
+}, 9);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# hexo-ob-admonition
+
+## Description
+It enables proper rendering of Obsidian notes in Hexo.
